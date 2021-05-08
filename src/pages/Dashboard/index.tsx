ebbfce3056ea1afe20b0100fe859435d8a382d59
RafaelSilvaMenezes <RafaelSilvaMenezes@outlook.com.br>
2021-05-08 18:14:03 -0300

    Finalizando app module - 30%

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,13 @@
+import React, { useCallback, useRef, useState, useEffect } from 'react';
+
+import { Container } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <Container>
+      <h1>Dashboard</h1>
+    </Container>
+  );
+};
+
+export default Dashboard;
